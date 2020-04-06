4ea5b3baf1685afa70f26493e7b754146d4ab355
aaronshivers <aaron@moonmail.org>
2020-04-05 22:58:34 -0500

    setup navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrimaryComponent } from './primary/primary.component';
+import { SecondaryComponent } from './secondary/secondary.component';
+import { TertiaryComponent } from './tertiary/tertiary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    PrimaryComponent,
+    SecondaryComponent,
+    TertiaryComponent
   ],
   imports: [
     BrowserModule
